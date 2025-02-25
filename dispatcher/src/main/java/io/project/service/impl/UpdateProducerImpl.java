bd2e22338481bd7854cf1c28ce873db6ce50cdcd
jokerSharp <qasoftware@tutanota.com>
2025-02-25 08:07:30 +0300

    add rabbitmq

--- dispatcher/src/main/java/io/project/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/io/project/service/impl/UpdateProducerImpl.java
@@ -1,16 +1,22 @@
 package io.project.service.impl;
 
 import io.project.service.UpdateProducer;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
