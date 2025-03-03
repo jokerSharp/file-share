20e89a8eb4b15b6774a1268623dc78ad7a33d833
jokerSharp <qasoftware@tutanota.com>
2025-03-03 19:59:45 +0300

    add a service to send a registration mail

---
+++ mail-service/src/main/java/io/project/dto/MailParams.java
@@ -0,0 +1,14 @@
+package io.project.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MailParams {
+
+    private String id;
+    private String emailTo;
+}
